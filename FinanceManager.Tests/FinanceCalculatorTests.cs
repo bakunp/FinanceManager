cfce270e586d8fe6a5639ee888e7c8978206cea1
bakunp <bakunp99@gmail.com>
2026-01-17 16:45:54 +0100

    Initialize xUnit test project and implement first unit test for FinanceCalculator

---
+++ FinanceManager.Tests/FinanceCalculatorTests.cs
@@ -0,0 +1,32 @@
+﻿using FinanceManager.Core;
+using Xunit;
+
+namespace FinanceManager.Tests
+{
+    public class FinanceCalculatorTests
+    {
+        private readonly FinanceCalculator _calculator;
+        public FinanceCalculatorTests()
+        {
+            _calculator = new FinanceCalculator();
+        }
+
+
+        [Fact]
+        public void CalculatePrioritySumTestExpectPass()
+        {
+            var goals = new List<Goal>
+            {
+                new() {Name = "TestGoalPriorityLow", Priority = Goal.GoalPriorityEnum.Low},
+                new() {Name = "TestGoalPriorityNormal", Priority = Goal.GoalPriorityEnum.Normal},
+                new() {Name = "TestGoalPriorityMedium", Priority = Goal.GoalPriorityEnum.Medium},
+                new() {Name = "TestGoalPriorityHigl", Priority = Goal.GoalPriorityEnum.High},
+                new() {Name = "TestGoalPriorityCritical", Priority = Goal.GoalPriorityEnum.Critical},
+            };
+
+            var result = _calculator.CalculatePrioritySum(goals);
+
+            Assert.Equal(19, result);
+        }
+    }
+}

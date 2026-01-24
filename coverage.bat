b18723a39a8d5287bf4c2a631ed40682de8c93f5
bakunp <bakunp99@gmail.com>
2026-01-24 17:42:56 +0100

    Improved the coverage script by excluding migrations

--- coverage.bat
+++ coverage.bat
@@ -2,7 +2,7 @@
 if exist TestResults rmdir /s /q TestResults
 if exist CoverageReport rmdir /s /q CoverageReport
 
-dotnet test --collect:"XPlat Code Coverage" --results-directory "TestResults"
+dotnet test --collect:"XPlat Code Coverage" --settings coverage.runsettings --results-directory "TestResults"
 
 reportgenerator -reports:TestResults\**\coverage.cobertura.xml -targetdir:CoverageReport -reporttypes:Html
 
